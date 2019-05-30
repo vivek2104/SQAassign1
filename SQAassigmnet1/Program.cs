a1613fa97c1e2efa6271d98f37787ef3e585d41b
Vivek Katroju <Vkatroju5947@conestogac.on.ca>
2019-05-30 01:49:01 -0400

    updated rectangle file

--- SQAassigmnet1/Program.cs
+++ SQAassigmnet1/Program.cs
@@ -1,15 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SQAassigmnet1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+﻿
--- SQAassigmnet1/RectangleClass.cs
+++ SQAassigmnet1/RectangleClass.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SQAassigmnet1
+namespace SQAassign1
 {
     public class rectangleClass
     {
